e3d406e3466a20c8072f3827d6c3397656458294
zhiaiing <2736834533@qq.com>
2017-05-04 15:43:26 +0800

    Add files via upload

---
+++ NSMutableArray+WDMutableArray.m
@@ -0,0 +1,36 @@
+
+
+#import "NSMutableArray+WDMutableArray.h"
+#import <objc/runtime.h>
+
+@implementation NSMutableArray (WDMutableArray)
+
++ (void)load {
+    Method fromMethod = class_getInstanceMethod(objc_getClass("__NSArrayM"), @selector(objectAtIndex:));
+    Method toMethod = class_getInstanceMethod(objc_getClass("__NSArrayM"), @selector(wd_objectAtIndex:));
+    method_exchangeImplementations(fromMethod, toMethod);
+}
+
+// 为了避免和系统的方法冲突，我一般都会在swizzling方法前面加前缀
+- (id)wd_objectAtIndex:(NSUInteger)index {
+    // 判断下标是否越界，如果越界就进入异常拦截
+    if (self.count-1 < index) {
+        @try {
+            return [self wd_objectAtIndex:index];
+        }
+        @catch (NSException *exception) {
+            // 在崩溃后会打印崩溃信息。
+            NSLog(@"---------- %s Crash Because Method %s  ----------\n", class_getName(self.class), __func__);
+            NSLog(@"%@", [exception callStackSymbols]);
+            
+            
+            return nil;
+        }
+        @finally {}
+    } // 如果没有问题，则正常进行方法调用
+    else {
+        return [self wd_objectAtIndex:index];
+    }
+}
+
+@end
